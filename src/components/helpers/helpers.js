c03af904692e285a8d15c9a2a19bf39ba87d9c7d
Benjamin Ufeh <olomufeh@gmail.com>
2025-01-04 21:19:50 +0100

    updating helpers.js

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,8 +1,8 @@
 export default {
     // testing 
-    BaseUrl: 'http://localhost:3005/api/v1/',
+    // BaseUrl: 'http://localhost:3005/api/v1/',
 
 
     // production 
-    // BaseUrl: 'https://serene-self-api.onrender.com/api/v1/'
+    BaseUrl: 'https://serene-self-api.onrender.com/api/v1/'
 }
